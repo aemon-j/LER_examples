977db1c2e5d54d9d3655f26755ba916dc390b6ae
Stefano Zaghi <johannes.feldbauer@posteo.de>
2021-03-16 14:35:58 +0100

    updated config files

---
+++ LoughFeeagh/run_lakeensemblr.R
@@ -0,0 +1,28 @@
+#initial clean up
+rm(list = ls())
+graphics.off()
+cat("\f")
+
+# set working directory
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+
+library(LakeEnsemblR)
+
+# Set config file
+config_file <- 'LakeEnsemblR.yaml'
+model <- c("FLake", "GLM", "GOTM", "Simstrat", "MyLake")
+# 1. Example - creates directories with all model setup
+export_config(config_file = config_file, model = c("FLake", "GLM", "GOTM", "Simstrat", "MyLake"),
+              folder = ".")
+# 2. Run ensemble lake models
+run_ensemble(config_file = config_file,
+             model = c("FLake", "GLM", "GOTM", "Simstrat", "MyLake"),
+             return_list = FALSE, parallel = FALSE)
+
+
+## Plot model output using gotmtools/ggplot2
+# Extract names of all the variables in netCDF
+ncdf <- 'output/ensemble_output.nc'
+
+plot_heatmap(ncdf)
